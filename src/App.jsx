17f455f2e05e54f97782e5ee0912dbda98171d11
ThamizhAnban <thamizhanban2006@gmail.com>
2025-02-19 10:31:51 +0530

    completed

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div>
+      <h1>Homepage</h1><hr/>
+<div className="details">     <div>
+          Name : M.Thamizhanban
+      </div>
+      <div>
+          Email : thamizhanban2006@gmail.com
+      </div>
+      <div>
+          Contact : 6380610185
+      </div>
+      </div>
+     
+    </div>
+  )
+}
+
+export default App
